95ef3097e193de472413d093afc7c48d1646133e
DmitryHudrich <dimaprudnikov9@gmail.com>
2023-09-20 00:15:59 +0300

    Тесты в ConsoleTest! настюля, я тебя люблю кстати очень

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Library.Collections.Vector;
+
+// Ну хоть что то
+var vec1 = new Vector<int>();
+var vec2 = new Vector<int>(new int[] { 2, 4, 5, 6, 7 });
